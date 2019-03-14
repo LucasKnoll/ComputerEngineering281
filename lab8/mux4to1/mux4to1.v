e83cf9be4a867077a6631f212764d84b9b4a0374
Lucas Knoll <40204374+LucasKnoll@users.noreply.github.com>
2019-03-13 19:25:08 -0500

    Add files via upload

---
+++ lab8/mux4to1/mux4to1.v
@@ -0,0 +1,9 @@
+module mux4to1(s1, s0, W0, W1, W2, W3, f);
+
+	input W0, W1, W2, W3, s1, s0;
+	output f;
+
+	assign f = s1 ? (s0 ? W0 : W1) : (s0 ? W2 : W3);
+	//assign f = (~s1&s0&W3)|(~s1&s0&W1)|(s1&~s0&W2)|(s1&s0&W3);
+
+endmodule
